6d142cd21c8b153bf559e6dcf7a1a80e0dc56bd4
AmyGois <ahagois@hotmail.com>
2024-11-08 17:55:17 +0000

    build: set up project dependencies
    Babel & Jest

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+const presets = [
+  [
+    "@babel/preset-env",
+    {
+      targets: { node: "current" },
+      useBuiltIns: "usage",
+      corejs: "3.6.4",
+    },
+  ],
+];
+
+module.exports = { presets };
